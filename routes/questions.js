d46c0cdc7d16fbc831373bb51e727eb3fdd03de7
Paul-Eirik <pmelhus@gmail.com>
2022-04-13 14:53:54 -0400

    added fields to question form and new mixins on utils. also added restoreUser in auth

--- routes/questions.js
+++ routes/questions.js
@@ -12,8 +12,35 @@ router.get("/", asyncHandler(async(req, res) => {
     res.render("questions", { questions })
 }));
 
-router.get('/new', csrfProtection, asyncHandler(async(req, res) => {
-    res.render('question-form', {csrfToken: req.csrfToken()})
+router.get('/new', asyncHandler(async(req, res) => {
+    // TODO: pass in tags to render
+    res.render('question-form')
+}))
+
+const questionValidators = [
+    check("title")
+      .exists({ checkFalsy: true })
+      .withMessage("Please provide a value for title")
+      .isLength({ max: 255 })
+      .withMessage("Title must not be more than 255 characters long."),
+    check("body")
+      .exists({ checkFalsy: true })
+      .withMessage("Please provide a value for body")
+]
+
+router.post('/new', questionValidators, asyncHandler(async(req, res) => {
+    const {title, body} = req.body
+    const validationErrors = validationResult(req);
+    if (validationErrors.isEmpty()) {
+        const question = await Question.create({title, body})
+        res.redirect('/questions')
+    } else {
+        const errors = validationErrors.array().map((error) => error.msg);
+        res.render("questions-form", {
+            question,
+            errors,
+          });
+    }
 }))
 
 module.exports = router;

11f30b0c4061e360954f74a9efb323f3ed2034e4
Paul-Eirik <pmelhus@gmail.com>
2022-04-13 09:36:11 -0400

    commented out comments in questions.pug, modified userName and password in user-login.pug to fix bug

--- routes/answers.js
+++ routes/answers.js
@@ -6,23 +6,21 @@ const { Question, Answer, User } = require("../db/models");
 const { asyncHandler, handleValidationErrors } = require("../utils");
 const csrfProtection = csrf({ cookie: true });
 
-
 const validateAnswers = [
-    //  Task name cannot be empty:
-    check("answer")
-        .exists({ checkFalsy: true })
-        .withMessage("Answer Body can't be empty.")
-]
+  //  Task name cannot be empty:
+  check("answer")
+    .exists({ checkFalsy: true })
+    .withMessage("Answer Body can't be empty."),
+];
 
-
-router.get("/", asyncHandler(async(req,res)=>{
+router.get(
+  "/",
+  asyncHandler(async (req, res) => {
     //TODO: add the correct router once this is working with front end
     //split the router later maybe?
-    const answers = await Answer.findAll({include: [User, Question]})
-    res.render("answers", { answers })
-}))
-
-
-
+    const answers = await Answer.findAll({ include: [User, Question] });
+    res.render("answers", { answers });
+  })
+);
 
 module.exports = router;

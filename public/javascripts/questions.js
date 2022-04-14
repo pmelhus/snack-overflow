aaf70c3f93d2294bb5650cf6eb77f21d5685969d
Paul-Eirik <pmelhus@gmail.com>
2022-04-14 14:48:37 -0400

    added delete button/functionality/confirmation page for individual questions

--- public/javascripts/questions.js
+++ public/javascripts/questions.js
@@ -1,60 +1,57 @@
 // const answer = require("../../db/models/answer");
 let count = 0;
-let answerText = 'answers'
+let answerText = "answers";
 
-window.addEventListener("DOMContentLoaded", ()=>{
-    const form = document.getElementsByClassName("form-field")
-    const newForm = Array.from(form)
-    const textArea = (newForm[0].children.body);
-    const listButtons = document.getElementsByTagName("button")
-    const answerList = document.getElementById('answer')
-    const answerCount = document.getElementById('answerId')
-    const questionLinks = document.querySelectorAll('h2 > a')
+window.addEventListener("DOMContentLoaded", () => {
+  const form = document.getElementsByClassName("form-field");
+  const newForm = Array.from(form);
+  const textArea = newForm[0].children.body;
+  const listButtons = document.getElementsByTagName("button");
+  const answerList = document.getElementById("answer");
+  const answerCount = document.getElementById("answerId");
+  const questionLinks = document.querySelectorAll("h2 > a");
 
-    // questionLinks.forEach((link) => {
-    //     const linkId = req.params.url.split('/')[2]
-    //     link.addEventListener('click', async(e) => {
-    //         const res = fetch(`/questions/${linkId}/answers`)
-    //         const data = res.json()
-    //         console.log(data)
-    //     })
-    // })
+  // questionLinks.forEach((link) => {
+  //     const linkId = req.params.url.split('/')[2]
+  //     link.addEventListener('click', async(e) => {
+  //         const res = fetch(`/questions/${linkId}/answers`)
+  //         const data = res.json()
+  //         console.log(data)
+  //     })
+  // })
 
+  listButtons[2].addEventListener("click", async (e) => {
+    e.preventDefault(); //stops reload on new answer button
+    //textArea.value = body text input
+    if (e) {
+      const res = await fetch("/answers/new", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          body: textArea.value,
+        }),
+      });
 
-    listButtons[2].addEventListener('click', async(e)=>{
-        e.preventDefault(); //stops reload on new answer button
-        //textArea.value = body text input
-        if(e){
-            const res = await fetch('/answers/new', {
-                method: "POST",
-                headers: {"Content-Type": "application/json"},
-                body: JSON.stringify({
-                    body: textArea.value
-                })
-            })
+      //const newAnswer = await res.json();
 
-            //const newAnswer = await res.json();
-
-            // if(res.status === 200){
-                const newDiv = document.createElement('div');
-                const newAnswer = document.createElement('p');
-                newAnswer.innerText = textArea.value;
-                textArea.value = '';
-                answerList.appendChild(newDiv);
-                newDiv.appendChild(newAnswer);
-                count++;
-                const updateAnswerCount = answerCount.children[0];
-                updateAnswerCount.value = count;
-                if (count === 1){
-                    answerText = 'answer'
-                    updateAnswerCount.innerHTML = `${count} ${answerText}`
-                } else {
-                    answerText = 'answers'
-                    updateAnswerCount.innerHTML = `${count} ${answerText}`
-                }
-            // }
-        } //add error for if the quesiton couldnt be retrieved
-    })
-
-
-})
+      // if(res.status === 200){
+      const newDiv = document.createElement("div");
+      const newAnswer = document.createElement("p");
+      newAnswer.innerText = textArea.value;
+      textArea.value = "";
+      answerList.appendChild(newDiv);
+      newDiv.appendChild(newAnswer);
+      count++;
+      const updateAnswerCount = answerCount.children[0];
+      updateAnswerCount.value = count;
+      if (count === 1) {
+        answerText = "answer";
+        updateAnswerCount.innerHTML = `${count} ${answerText}`;
+      } else {
+        answerText = "answers";
+        updateAnswerCount.innerHTML = `${count} ${answerText}`;
+      }
+      // }
+    } //add error for if the quesiton couldnt be retrieved
+  });
+});
--- routes/questions.js
+++ routes/questions.js
@@ -5,61 +5,100 @@ const { check, validationResult } = require("express-validator");
 const { Question, Answer, User } = require("../db/models");
 const { asyncHandler, handleValidationErrors } = require("../utils");
 const csrfProtection = csrf({ cookie: true });
-const { requireAuth } = require('../auth');
+const { requireAuth } = require("../auth");
 
-
-router.get("/", asyncHandler(async(req, res) => {
-
-    const questions = await Question.findAll( {include: [Answer, User]});
-    let answers = questions.map(async q => await Answer.findAll({where: {questionId: q.id}}))
+router.get(
+  "/",
+  asyncHandler(async (req, res) => {
+    const questions = await Question.findAll({ include: [Answer, User] });
+    let answers = questions.map(
+      async (q) => await Answer.findAll({ where: { questionId: q.id } })
+    );
     if (res.locals.authenticated) {
-    const loggedInUser = await User.findByPk(res.locals.user.id);
-    res.render("questions", { loggedInUser, questions, answers })
+      const loggedInUser = await User.findByPk(res.locals.user.id);
+      res.render("questions", { loggedInUser, questions, answers });
     } else {
-        res.render("questions", { questions, answers })
+      res.render("questions", { questions, answers });
     }
-}));
+  })
+);
 
-router.get('/new', requireAuth, asyncHandler(async(req, res) => {
-    const {question, user} = req.body
-    res.render('question-form', question, user)
-}))
+router.get(
+  "/new",
+  requireAuth,
+  asyncHandler(async (req, res) => {
+    const { question, user } = req.body;
+    res.render("question-form", question, user);
+  })
+);
 
 const questionValidators = [
-    check("title")
-      .exists({ checkFalsy: true })
-      .withMessage("Please provide a value for title")
-      .isLength({ max: 255 })
-      .withMessage("Title must not be more than 255 characters long."),
-    check("body")
-      .exists({ checkFalsy: true })
-      .withMessage("Please provide a value for body")
-]
+  check("title")
+    .exists({ checkFalsy: true })
+    .withMessage("Please provide a value for title")
+    .isLength({ max: 255 })
+    .withMessage("Title must not be more than 255 characters long."),
+  check("body")
+    .exists({ checkFalsy: true })
+    .withMessage("Please provide a value for body"),
+];
 
-router.post('/new', questionValidators, asyncHandler(async(req, res) => {
-    const {title, body} = req.body
-    console.log(res.locals)
+router.post(
+  "/new",
+  questionValidators,
+  asyncHandler(async (req, res) => {
+    const { title, body } = req.body;
+    console.log(res.locals);
     const validationErrors = validationResult(req);
     if (validationErrors.isEmpty()) {
-        const question = await Question.create({userId: res.locals.user.id, title, body})
-        res.redirect('/questions')
+      const question = await Question.create({
+        userId: res.locals.user.id,
+        title,
+        body,
+      });
+      res.redirect("/questions");
     } else {
-        const errors = validationErrors.array().map((error) => error.msg);
-        res.render("questions-form", {
-            question,
-            errors,
-          });
+      const errors = validationErrors.array().map((error) => error.msg);
+      res.render("questions-form", {
+        question,
+        errors,
+      });
     }
-}))
+  })
+);
 
-router.get('/:id(\\d+)', asyncHandler(async(req, res) => {
+router.get(
+  "/:id(\\d+)",
+  asyncHandler(async (req, res) => {
     const id = await req.params.id;
-    console.log(id);
-    const question = await Question.findByPk(id, {include: [Answer, User]});
-    const answers = await Answer.findAll({where: {questionId: question.id}})
-    const { questionId, body, answerScore, userId} = Answer
-    await Answer.build()
-    return res.render('question-page', {question, answers});
-}))
+    const loggedInUser = await User.findByPk(res.locals.user.id);
+    const question = await Question.findByPk(id, { include: [Answer, User] });
+    const answers = await Answer.findAll({
+      where: { questionId: question.id },
+    });
+    const { questionId, body, answerScore, userId } = Answer;
+    await Answer.build();
+    return res.render("question-page", { question, answers, id, loggedInUser });
+  })
+);
+
+router.get(
+  "/:id(\\d+)/delete",
+  asyncHandler(async (req, res) => {
+    const id = await req.params.id;
+
+    res.render("delete-confirmation", { id });
+  })
+);
+
+router.post(
+  "/:id(\\d+)/delete",
+  asyncHandler(async (req, res) => {
+    const questionId = parseInt(req.params.id, 10);
+    const question = await Question.findByPk(questionId);
+    question.destroy();
+    res.redirect("/questions");
+  })
+);
 
 module.exports = router;

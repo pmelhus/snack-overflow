35e7e9ce8130e1efc3191c5b6f5063540412780e
Anthony Bronca <95654116+AnthonyBronca@users.noreply.github.com>
2022-04-16 04:24:48 -0400

    Revert "added a 404 render page"

--- app.js
+++ app.js
@@ -56,10 +56,10 @@ app.use(function (err, req, res, next) {
 
   res.locals.message = err.message;
   res.locals.error = req.app.get("env") === "development" ? err : {};
-  res.render('404')
+
   // render the error page
-  // res.status(err.status || 500);
-  // res.render("error");
+  res.status(err.status || 500);
+  res.render("error");
 });
 
 module.exports = app;

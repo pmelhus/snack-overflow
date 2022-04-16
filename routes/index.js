2766edd39fadf8653e0d820e0c37fecb4995f37a
Paul-Eirik <pmelhus@gmail.com>
2022-04-16 19:49:24 -0400

    commented out search functions so website will work

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,10 @@
 const express = require('express');
-const { Op, sequelize } = require('sequelize')
+// const { Op, sequelize } = require('sequelize')
 const router = express.Router();
 const { User, Question, Answer } = require('../db/models');
 const { asyncHandler } = require('../utils');
-const { searchHelper } = require('../public/javascripts/search')
-// const { requireAuth } = require('../auth')
+// const { searchHelper } = require('../public/javascripts/search')
+const { requireAuth } = require('../auth')
 
 
 /* GET home page. */
@@ -19,29 +19,29 @@ router.get('/', asyncHandler(async(req, res, next)=>{
   }
 }));
 
-const searchResults = async(term) => {
-  const questions = await Question.findAll({
-    where: {
-      title: {
-        [Op.substring]: term
-      }
-    },
-    include: [Answer, User]
-  })
-  return questions
-}
+// const searchResults = async(term) => {
+//   const questions = await Question.findAll({
+//     where: {
+//       title: {
+//         [Op.substring]: term
+//       }
+//     },
+//     include: [Answer, User]
+//   })
+//   return questions
+// }
 
-router.get(searchHelper, asyncHandler(async(req, res, next) => {
-  let results = []
-  const rawQuery = req.params.url.split('?q=')[1].toString()
-  if (rawQuery.includes('%20')) {
-    const spacedQuery = rawQuery.split(spacer).join(' ')
-    results = await searchResults(spacedQuery)
-  } else {
-    results = await searchResults(rawQuery)
-  }
-  res.render('search', {title: 'Snack Search', results, rawQuery, spacedQuery})
-}))
+// router.get('/', searchHelper, asyncHandler(async(req, res, next) => {
+//   let results = []
+//   const rawQuery = req.params.url.split('?q=')[1].toString()
+//   if (rawQuery.includes('%20')) {
+//     const spacedQuery = rawQuery.split(spacer).join(' ')
+//     results = await searchResults(spacedQuery)
+//   } else {
+//     results = await searchResults(rawQuery)
+//   }
+//   res.render('search', {title: 'Snack Search', results, rawQuery, spacedQuery})
+// }))
 
 
 

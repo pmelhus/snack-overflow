d067f538aa0a031981c2227d7488252e60e14738
Paul-Eirik <pmelhus@gmail.com>
2022-04-16 19:58:50 -0400

    merged main

---
+++ public/javascripts/search.js
@@ -0,0 +1,12 @@
+
+// const searchInput = document.getElementById('searchInput')
+
+// const searchHelper = searchInput.addEventListener('keyup', async(e) => {
+//     const query = await searchInput.value
+
+//     const queryToURI = encodeURIComponent(query)
+//     const routeURI = `/search?q=${queryToURI}`
+//     return routeURI
+// })
+
+// module.exports = searchHelper;

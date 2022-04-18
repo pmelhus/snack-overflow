f339a2c0f93b26aee8dc1d547efea7958fa39dbc
Anthony Bronca <95654116+AnthonyBronca@users.noreply.github.com>
2022-04-17 22:40:52 -0400

    commented out non-working code

--- public/javascripts/login.js
+++ public/javascripts/login.js
@@ -1,17 +1,17 @@
-window.addEventListener('DOMContentLoaded', ()=>{
-    const demoLogin = document.getElementsByClassName('user-form-button')[2]
+// window.addEventListener('DOMContentLoaded', ()=>{
+//     const demoLogin = document.getElementsByClassName('user-form-button')[2]
 
 
-    demoLogin.addEventListener('click', async (e)=>{
-        e.preventDefault();
-        const res = await fetch('/users/demo/login', {
-            method: 'POST',
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify({
-                userName: 'demoUser',
-                password: 'DemoUserPassword123!'
-            })
-        })
-    })
+//     demoLogin.addEventListener('click', async (e)=>{
+//         e.preventDefault();
+//         const res = await fetch('/users/demo/login', {
+//             method: 'POST',
+//             headers: { "Content-Type": "application/json" },
+//             body: JSON.stringify({
+//                 userName: 'demoUser',
+//                 password: 'DemoUserPassword123!'
+//             })
+//         })
+//     })
 
-})
+// })
